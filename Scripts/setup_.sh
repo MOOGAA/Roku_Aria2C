9aa53c3f69b5be3814cf429c60d5ee5f60ce4c12
MOOGAA <arjunsinhchudasama7.6@gmail.com>
2021-09-01 13:17:47 +0530

    Update setup_.sh

--- Scripts/setup_.sh
+++ Scripts/setup_.sh
@@ -29,13 +29,15 @@ func_2() {
   
   curl -fsSL git.io/aria2c.sh | bash && \
   func_R && \
-  mkdir -p /home/app/Downloads && \
+  mkdir -p /home/app/dht/ && \
+  mkdir -p /home/app/Downloads/ && \
   wget -q \
-    https://github.com/P3TERX/aria2.conf/raw/master/dht.dat && \
+    https://github.com/P3TERX/aria2.conf/raw/master/dht.dat \
+      ~/dht/ && \
   wget -q \
-    https://github.com/P3TERX/aria2.conf/raw/master/dht6.dat && \
-  tracker_list=`curl -Ns https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt | awk '$1' | tr '\n' ',' | cat`
-  echo "bt-tracker=$tracker_list" >> aria2c.conf && \
+    https://github.com/P3TERX/aria2.conf/raw/master/dht6.dat \
+      ~/dht/ && \
+  echo "bt-tracker='[$tracker_list]'" >> aria2c.conf && \
   echo $PATH > PATH
   
 }
