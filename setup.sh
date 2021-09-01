7f9483b558f65b39e62b5804f96563224df77bed
MOOGAA <arjunsinhchudasama7.6@gmail.com>
2021-09-01 08:24:25 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
 
+
+
+###########################################################################################################################################################################################################################################################################################################################################################################################
+
 # Install rclone static binary
+
+func_1() {
+  
+  apt-get -qq update ; \
+  apt-get -qqy autoremove ; \
+  apt-get -qq update ; \
+  apt-get -qqy upgrade ;\
+  apt-get -qq update ; \
+  apt-get -qqy install --fix-missing --fix-broken ; \
+  apt-get -qq update
+  
+} &> /dev/null
+
+func_2() {
 wget -q https://downloads.rclone.org/v1.53.0/rclone-v1.53.0-linux-amd64.zip
 unzip -q rclone-v1.53.0-linux-amd64.zip
 export PATH=$PWD/rclone-v1.53.0-linux-amd64:$PATH
+}
+
+
 
 # Install aria2c static binary
 wget -q https://github.com/P3TERX/aria2-builder/releases/download/1.35.0_2020.09.04/aria2-1.35.0-static-linux-amd64.tar.gz
@@ -22,3 +43,14 @@ tracker_list=`curl -Ns https://raw.githubusercontent.com/ngosang/trackerslist/ma
 echo "bt-tracker=$tracker_list" >> aria2c.conf
 
 echo $PATH > PATH
+
+
+
+#
+W
+#
+###########################################################################################################################################################################################################################################################################################################################################################################################
+
+
+
+###
