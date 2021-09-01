7575311bf021e6f6c32c047ec40998d404414925
MOOGAA <arjunsinhchudasama7.6@gmail.com>
2021-09-01 12:46:54 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,7 +10,7 @@ fi
 export PATH="$(cat PATH)"
 
 #if [[ -n $RCLONE_CONFIG && -n $RCLONE_DESTINATION ]]; then
-if [[ -n Config/rclone.conf && -n $REMOTE_DST ]]; than
+if [[ -n /home/app/.config/rclone/rclone.conf && -n $REMOTE_DST ]]; than
         echo "Rclone config detected"
 	echo "on-download-stop=./Scripts/delete_.sh" >> aria2c.conf
 	echo "on-download-complete=./Scripts/on-complete_.sh" >> aria2c.conf
