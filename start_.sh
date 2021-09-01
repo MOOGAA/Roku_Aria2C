f27a248a53d3943db9a45e9c35ff7c1b6eec9234
MOOGAA <arjunsinhchudasama7.6@gmail.com>
2021-09-01 13:34:49 +0530

    Update start_.sh

--- start_.sh
+++ start_.sh
@@ -13,11 +13,12 @@ export PATH="$(cat PATH)"
 if [[ -n /home/app/.config/rclone/rclone.conf && -n $REMOTE_DST ]]; than
         echo "Rclone config detected"
 	echo "on-download-stop=./Scripts/delete_.sh" >> aria2c.conf
-	echo "on-download-complete=./Scripts/on-complete_.sh" >> aria2c.conf
+	echo "on-download-complete=./Scrip/on-complete_.sh" >> aria2c.conf
 	chmod +x delete.sh
 	chmod +x on-complete.sh
 fi
 
 echo "rpc-secret=$ARIA2C_PASSWD" >> aria2c.conf
+./setup_.sh
 aria2c --conf-path=aria2c.conf &
 yarn start
