a6a75ed3dfb0f3b2e51940f2a02e475878a0ace2
MOOGAA <arjunsinhchudasama7.6@gmail.com>
2021-09-02 16:05:25 +0530

    Update start_.sh

--- start_.sh
+++ start_.sh
@@ -1,26 +1,5 @@
 #!/bin/bash
 
-func_tmp() {
-if [ -f .env ]; then
-    echo ".env file found, sourcing it"
-	set -o allexport
-	source .env
-	set +o allexport
-fi
-
-export PATH="$(cat PATH)"
-
-#if [[ -n $RCLONE_CONFIG && -n $RCLONE_DESTINATION ]]; then
-if [[ -n /home/app/.config/rclone/rclone.conf && -n $REMOTE_DST ]]; than
-        echo "Rclone config detected"
-	echo "on-download-stop=~/Scripts/delete_.sh" >> aria2c.conf
-	echo "on-download-complete=~/Scripts/on-complete_.sh" >> aria2c.conf
-	chmod +x ~/Scripts/delete.sh
-	chmod +x ~/Scripts/on-complete.sh
-fi
-}
-
-
 echo "rpc-secret=$ARIA2C_PASSWD" >> aria2c.conf
 aria2c --conf-path=aria2c.conf &
 yarn start
