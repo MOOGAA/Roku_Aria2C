487f2c7104e86433c2ab9b439b82c78368e3c5d4
MOOGAA <arjunsinhchudasama7.6@gmail.com>
2021-09-04 02:27:43 +0530

    Update on-complete_.sh

--- Scripts/on-complete_.sh
+++ Scripts/on-complete_.sh
@@ -27,9 +27,9 @@ echo -e "$(date +"%m/%d %H:%M:%S") ${INFO} Delete .aria2 file finish"
 echo "$(($(cat numUpload)+1))" > numUpload # Plus 1
 
 if [[ $2 -eq 1 ]]; then # single file
-	rclone -v --config="/home/app/.config/rclone/rclone.conf" copy "$3" "$REMOTE:$RCLONE_DESTINATION" 2>&1	
+	rclone -v --config="/root/.config/rclone/rclone.conf" copy "$3" "$REMOTE:$RCLONE_DESTINATION" 2>&1	
 elif [[ $2 -gt 1 ]]; then # multiple file
-	rclone -v --config="/home/app/.config/rclone/rclone.conf" copy "$topPath" "$REMOTE:$RCLONE_DESTINATION/${relativePath%%/*}"
+	rclone -v --config="/root/.config/rclone/rclone.conf" copy "$topPath" "$REMOTE:$RCLONE_DESTINATION/${relativePath%%/*}"
 fi
 
 echo "$(($(cat numUpload)-1))" > numUpload # Minus 1
