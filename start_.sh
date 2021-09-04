1529dbad786a4cedb92b5f4c4c54cc7df0a99d71
MOOGAA <arjunsinhchudasama7.6@gmail.com>
2021-09-04 20:01:17 +0530

    Update start_.sh

--- start_.sh
+++ start_.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd && \
 echo "rpc-secret=$ARIA2C_PASSWD" >> ./.aria2/aria2c.conf && \
-rclone tree ~/ && \
+rclone tree -a ~/ && \
 aria2c --conf-path=./.aria2/aria2c.conf &
 yarn start
